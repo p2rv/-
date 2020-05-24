6c47e213282ec1433f79096a43efe00e243cf952
Pavel1 <pros_pavlo@mail.ru>
2020-05-24 12:23:37 +0900

    Initial commit

--- МорскойБой/MainWindow.xaml.cs
+++ МорскойБой/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace МорскойБой
+namespace Battleships
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
--- МорскойБой/Properties/Settings.Designer.cs
+++ МорскойБой/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace МорскойБой.Properties
+namespace Battleships.Properties
 {
 
 
