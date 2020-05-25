99fd575f7c4ec9e6fa2a539fc40c6277d60bc2eb
Pavel <pros_pavlo@mail.ru>
2020-05-25 09:09:16 +0900

    Добавлен обработчик вввода IP

--- МорскойБой/NewGameWindow.xaml.cs
+++ МорскойБой/NewGameWindow.xaml.cs
@@ -40,5 +40,51 @@ namespace Battleships
             tb_ip.Text = IP;
             
         }
+        private List<int> GetAddressBytes(string ip_str)
+        {
+            List<int> ip_octets=new List<int>();
+            string[] ip = ip_str.Split(new char[] { '.' });
+            foreach(string octet in ip)
+            {
+                int val;
+                if (Int32.TryParse(octet, out val))
+                    ip_octets.Add(val);
+            }
+            return ip_octets;
+        }
+        private void Tb_ip_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            List<int> ip_octets;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != ".")
+            {
+                e.Handled = true; // отклоняем ввод
+                return;
+            }
+            ip_octets = GetAddressBytes(tb_ip.Text+e.Text);
+            if (ip_octets.Last() > 255)
+            {
+                e.Handled = true;
+                return;
+            }
+           
+
+        }
+
+        private void Tb_ip_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true; // если пробел, отклоняем ввод
+            }
+        }
+
+        private void Tb_ip_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //List<byte> ip_octets;
+            //ip_octets = GetAddressBytes(tb_ip.Text);
+            //if (ip_octets.Last() >= 100 && tb_ip.Text.Last()!='.')
+            //    tb_ip.Text +=  ".";
+        }
     }
 }
